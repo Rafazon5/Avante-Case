633ceddd5316ef31c904caf81cca62e42e6ed7ea
Rafael Vitor <168605431+Rafazon5@users.noreply.github.com>
2025-11-04 18:43:22 -0300

    Correção da variável do pino IR e habilitando a saída serial.
    Fix variable name for IR pin and add serial output.

--- ir.cpp
+++ ir.cpp
@@ -4,10 +4,13 @@ int ledPin = 2;    // pino do LED
 void setup() {
   pinMode(irPin, INPUT); //ir como entrada
   pinMode(ledPin, OUTPUT); //led como saida
+  Serial.begin(9600);
 }
 
 void loop() {
-  int valorIR = digitalRead(pirPin);
+  int valorIR = digitalRead(irPin);
+  Serial.println(valorIR);
+  
   if (valorIR == 0) { //ir é ao contrario, se tem aproximacao ele fica em 0
     digitalWrite(ledPin, 1);
   } else {

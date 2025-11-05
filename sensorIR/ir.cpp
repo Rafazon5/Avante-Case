76b74112191d1068d44abae05ca12aab07324ba1
Igor Filho <igorjbmfilho@gmail.com>
2025-11-05 14:24:09 -0300

    Add IR sensor and LED control functionality

--- sensorIR/ir.cpp
+++ sensorIR/ir.cpp
@@ -1,1 +1,19 @@
+int irPin = 9;    // pino de sinal do IR
+int ledPin = 2;    // pino do LED
 
+void setup() {
+  pinMode(irPin, INPUT); //ir como entrada
+  pinMode(ledPin, OUTPUT); //led como saida
+  Serial.begin(9600);
+}
+
+void loop() {
+  int valorIR = digitalRead(irPin);
+  Serial.println(valorIR);
+  
+  if (valorIR == 0) { //ir é ao contrario, se tem aproximacao ele fica em 0
+    digitalWrite(ledPin, 1);
+  } else {
+    digitalWrite(ledPin, 0);
+  }
+}
